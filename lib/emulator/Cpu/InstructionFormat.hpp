ba10f173e3d719838c8a1d45116015425ac0f758
Domenico Iezzi <domenico@iezzi.info>
2025-05-24 13:09:30 +0000

    Move contstructor of InstructionFormat classes constexpr

--- lib/emulator/Cpu/InstructionFormat.hpp
+++ lib/emulator/Cpu/InstructionFormat.hpp
@@ -10,7 +10,14 @@ struct Bol {
     RegValue b;
     RegValue off16;
 
-    explicit Bol(const RegValue& value);
+    constexpr explicit Bol(const RegValue& value)
+    : a(value.extract32(8_offset, 4))
+    , b(value.extract32(12_offset, 4))
+    , off16(value.extract32(16_offset, 6)
+          | (value.extract32(28_offset, 4) << 6_regval)
+          | (value.extract32(22_offset, 6) << 10_regval))
+{
+}
 };
 
 struct Rr {
@@ -18,7 +25,12 @@ struct Rr {
     RegValue b;
     RegValue c;
 
-    explicit Rr(const RegValue& value);
+    constexpr explicit Rr(const RegValue& value)
+        : a(value.extract32(8_offset, 4))
+        , b(value.extract32(12_offset, 4))
+        , c(value.extract32(28_offset, 4))
+    {
+    }
 };
 
 struct Brn {
@@ -26,7 +38,12 @@ struct Brn {
     RegValue n;
     RegValue disp15;
 
-    explicit Brn(const RegValue& value);
+    constexpr explicit Brn(const RegValue& value)
+        : a(value.extract32(8_offset, 4))
+        , n(value.extract32(12_offset, 4))
+        , disp15(value.extract32(16_offset, 15))
+    {
+    }
 };
 
 } // namespace Tricore::InstructionFormat
