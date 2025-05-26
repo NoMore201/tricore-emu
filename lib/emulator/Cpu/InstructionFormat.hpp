be326ec4aa22499b2434cb836b0219bb0697f023
Domenico Iezzi <domenico.iezzi1@schindler.com>
2025-05-26 08:45:49 +0000

    Remove Register class

--- lib/emulator/Cpu/InstructionFormat.hpp
+++ lib/emulator/Cpu/InstructionFormat.hpp
@@ -1,80 +1,60 @@
 #ifndef TRICORE_EMU_INSTRUCTION_FORMAT_HPP
 #define TRICORE_EMU_INSTRUCTION_FORMAT_HPP
 
-#include "Cpu/Registers.hpp"
+#include "Utils.hpp"
 
 namespace Tricore::InstructionFormat {
 
 struct Bol {
-    RegValue a;
-    RegValue b;
-    RegValue off16;
+    u32 a;
+    u32 b;
+    u32 off16;
 
     constexpr explicit Bol(u32 insn)
-        : Bol(RegValue { insn })
-    {
-    }
-
-    constexpr explicit Bol(const RegValue& value)
-        : a(value.extract32(8_offset, 4))
-        , b(value.extract32(12_offset, 4))
-        , off16(value.extract32(16_offset, 6)
-              | (value.extract32(28_offset, 4) << 6_regval)
-              | (value.extract32(22_offset, 6) << 10_regval))
+        : a(Utils::extract32(insn, 8, 4))
+        , b(Utils::extract32(insn, 12, 4))
+        , off16(Utils::extract32(insn, 16, 6)
+              | (Utils::extract32(insn, 28, 4) << 6)
+              | (Utils::extract32(insn, 22, 6) << 10))
     {
     }
 };
 
 struct Rr {
-    RegValue a;
-    RegValue b;
-    RegValue c;
+    u32 a;
+    u32 b;
+    u32 c;
 
     constexpr explicit Rr(u32 insn)
-        : Rr(RegValue { insn })
-    {
-    }
-
-    constexpr explicit Rr(const RegValue& value)
-        : a(value.extract32(8_offset, 4))
-        , b(value.extract32(12_offset, 4))
-        , c(value.extract32(28_offset, 4))
+        : a(Utils::extract32(insn, 8, 4))
+        , b(Utils::extract32(insn, 12, 4))
+        , c(Utils::extract32(insn, 28, 4))
     {
     }
 };
 
 struct Brn {
-    RegValue a;
-    RegValue n;
-    RegValue disp15;
+    u32 a;
+    u32 n;
+    u32 disp15;
 
     constexpr explicit Brn(u32 insn)
-        : Brn(RegValue { insn })
-    {
-    }
-
-    constexpr explicit Brn(const RegValue& value)
-        : a(value.extract32(8_offset, 4))
-        , n(value.extract32(12_offset, 4))
-        , disp15(value.extract32(16_offset, 15))
+        : a(Utils::extract32(insn, 8, 4))
+        , n(Utils::extract32(insn, 12, 4))
+        , disp15(Utils::extract32(insn, 16, 15))
     {
     }
 };
 
 struct Rc {
-    RegValue a;
-    RegValue c;
-    RegValue const9;
+    u32 a;
+    u32 c;
+    u32 const9;
 
     constexpr explicit Rc(u32 insn)
-        : Rc(RegValue { insn })
-    {
-    }
-
-    constexpr explicit Rc(const RegValue& value)
-        : a(value.extract32(8_offset, 4))
-        , c(value.extract32(28_offset, 4))
-        , const9(value.extract32(12_offset, 9))
+        : a(Utils::extract32(insn, 8, 4))
+        , c(Utils::extract32(insn, 28, 4))
+        , const9(Utils::extract32(insn, 12, 9))
     {
     }
 };
